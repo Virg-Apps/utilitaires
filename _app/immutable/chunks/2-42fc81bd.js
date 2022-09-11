60251e4db4702f0a92d0736129d02ee176a53ddf
virgile de Lacerda <v.delacerda@virgapps.net>
2022-09-11 21:08:36 +0200

    Updates

---
+++ _app/immutable/chunks/2-42fc81bd.js
@@ -0,0 +1,1 @@
+import{default as t}from"../components/pages/_page.svelte-9249445d.js";export{t as component};
---
+++ _app/immutable/chunks/3-59420a6a.js
@@ -0,0 +1,1 @@
+import{default as t}from"../components/pages/sante/_page.svelte-385ac42e.js";export{t as component};
